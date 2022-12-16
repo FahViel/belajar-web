1ebe4dcb438a700f72db771b3a554677ff3d4636
FahViel <fahrimaulana007@gmail.com>
2022-12-16 13:03:06 +0700

    Merefactoring kode di script.js agar lebih efisien

--- Javascript/latihan-7/script.js
+++ Javascript/latihan-7/script.js
@@ -1,15 +1,5 @@
 function jumlahVolumeDuaKubus(a, b) {
-    var volumeA;
-    var volumeB;
-    var total;
-
-    volumeA = a * a * a;
-    volumeB = b * b * b;
-
-    total = volumeA + volumeB;
-
-    return total;
+  return = ( a * a * a ) + ( b * b * b);
 }
 
-console.log(jumlahVolumeDuaKubus(8, 3));
-console.log(jumlahVolumeDuaKubus(18, 30));
+console.log(jumlahVolumeDuaKubus(8, 3))
