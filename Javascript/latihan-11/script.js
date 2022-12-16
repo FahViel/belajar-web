33c4955a7f0c5b80d6f3c5bf4afb2e41cc6ba441
FahViel <fahrimaulana007@gmail.com>
2022-12-16 16:23:38 +0700

    Menambahkan kode untuk belajar javascript array manipulation

---
+++ Javascript/latihan-11/script.js
@@ -0,0 +1,39 @@
+// Manipulasi Array
+
+// 1. Menambah isi Array
+
+// var arr = ["a", 1, true];
+
+// console.log(arr);
+
+// 2. Menghapus isi Array
+
+// var arr = ["Muhammad", "Fahri", "Maulana"];
+// arr[1] = undefined;
+
+// console.log(arr);
+
+//3. Menampilkan isi array
+// var arr = ["Muhammad", "Fahri", "Maulana"];
+
+// for (var i =0; i < arr.length; i++ ){
+//     console.log('Mahasiswa ke-' + (i+1) + ' : ' + arr[i]);
+    
+// }
+
+// Method pada Array
+
+
+var arr = ["Muhammad", "Fahri", "Maulana"];
+// 1. Join
+    // console.log(arr.join(' '));
+
+// 2. Push & Pop
+// arr.push("Ari");
+// arr.pop();
+
+// 3. Unshift & Shift
+// arr.unshift('Ari');
+// arr.shift();
+
+console.log(arr.join(' '));
