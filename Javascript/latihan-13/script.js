b7eaa5324cfcd25c4e2e6834e9dcdbbbe5bc4d49
FahViel <fahrimaulana007@gmail.com>
2022-12-18 13:38:08 +0700

    Menambahkan kode hasil belajar pengenalan javascript object

---
+++ Javascript/latihan-13/script.js
@@ -0,0 +1,12 @@
+// membuat Object
+
+var mhs = {
+    nama : "fahri maulana",
+    umur : 25,
+    ips : [3.00, 2.50, 3.20],
+    alamat : {
+        jalan : "Bluru",
+        kota : "Sidoarjo",
+        provinsi : "Jatim"
+    }
+};
