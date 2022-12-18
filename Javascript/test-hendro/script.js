82d92c35f4e799c3661a8dfe29f181a3b921f8e9
FahViel <fahrimaulana007@gmail.com>
2022-12-18 14:38:43 +0700

    Merevisi file test hendro tanpa menggunakan function dan menambahkan hasil belajar

--- Javascript/test-hendro/script.js
+++ Javascript/test-hendro/script.js
@@ -1,16 +1,32 @@
+// Sorting Ascending
 const nomor1 = [2, 5, 9, 1, 7];
 
-nomor1.sort(function(a, b) {
-  return a - b;
-});
+for ( i = 0; i < nomor1.length; i++) {
+  for ( j = 0; j < nomor1.length - i - 1; j++) {
+    if (nomor1[j] > nomor1[j + 1]) {
+      const temp = nomor1[j];
+      nomor1[j] = nomor1[j + 1];
+      nomor1[j + 1] = temp;
+    }
+  }
+}
 
 console.log(nomor1);
 
+// Sorting Descending
+
 const nomor2 = [2, 5, 9, 1, 7];
 
-nomor2.sort(function(a, b) {
-  return b - a;
-});
+for ( i = 0; i < nomor2.length; i++) {
+  for ( j = 0; j < nomor2.length - i - 1; j++) {
+    if (nomor2[j] < nomor2[j + 1]) {
+      const temp = nomor2[j];
+      nomor2[j] = nomor2[j + 1];
+      nomor2[j + 1] = temp;
+    }
+  }
+}
 
 console.log(nomor2);
+
 
