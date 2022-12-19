3687840c0116794729840b98d68923d28e76bf39
FahViel <fahrimaulana007@gmail.com>
2022-12-19 15:33:58 +0700

    Menambahkan kode hasil belajar javascript object

--- Javascript/latihan-16/script.js
+++ Javascript/latihan-16/script.js
@@ -1,63 +1,32 @@
-var penumpang = [];
-var tambahPenumpang = function (namaPenumpang, penumpang) {
-  // jika angkot kosong
-  if (penumpang.length == 0) {
-    //tambah penumpang di awal array
-    penumpang.push(namaPenumpang);
-    //kembalikan isi array & keluar dari function
-    return penumpang;
-  } else {
-    // telusuri seluruh kursi dari awal
-    for (var i = 0; i < penumpang.length; i++) {
-      // jika ada kursi kosong
-      if (penumpang[i] == 0) {
-        // tambah penumpang di kursi tersebut
-        penumpang[i] = namaPenumpang;
-        // kembalikan isi array & keluar dari function
-        return penumpang;
+// membuat Object Angkot
+
+function Angkot(sopir, trayek, penumpang, kas) {
+    this.sopir = sopir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas;
+    
+    this.penumpangNaik = function (namaPenumpang) {
+      this.penumpang.push(namaPenumpang);
+      return this.penumpang;
+    }
+    this.penumpangTurun = function(namaPenumpang, bayar){
+      if ( this.penumpang.length === 0) {
+        alert('Angkot Masih Kosong!');
+        return false;
       }
-      // jika sudah ada nama penumpang yang sama
-      else if (penumpang[i] == namaPenumpang) {
-        // tampilkan pesan kesahalannya
-        console.log(namaPenumpang + ' sudah ada di dalam angkot.')
-        // kembalikan isi array & keluar dari function
-        return penumpang;
-      } else if (i == penumpang.lenght - 1) {
-        // tambah penumpang di akhir array
-        penumpang.push(namaPenumpang)
-        // kembalikan isi array & keluar dari function
-        return penumpang;
+
+      for( var i = 0; i < this.penumpang.length; i++){
+        if ( this.penumpang[i] == namaPenumpang) {
+          this.penumpang[i] = undefined;
+          this.kas += bayar;
+          return this.penumpang;
+        }
       }
     }
-  }
 }
 
-var hapusPenumpang = function (namaPenumpang, penumpang) {
-  // jika angkot kosong
-  if (penumpang.lenght == 0) {
-    // tampilkan pesan bahwa angkot kosong, dan
-    // tidak mungkin ada penumpang turun
-    console.log('Angkot masih kosong.');
-    // kembalikan isi array & keluar dari function
-    return penumpang;
-  } else {
-    // telusuri seluruh kursi dari awal
-    for (var i = 0; i < penumpang.lenght; i++) {
-      //jika nama penumpang sesuai
-      if (penumpang[i] == namaPenumpang) {
-        // hapus penumpang dengan mengubah namanya,
-        // menjadi undefined
-        penumpang[i] = 0;
-        // kembalikan isi array & keluar dari function
-        return penumpang;
-      }
-      // jika tidak ada nama yang sesuai
-      else if (i == penumpang.lenght - 1) {
-        // tampilkan pesan kesalahannya
-        console.log(namaPenumpang + ' tidak ada di dalam angkot.');
-        // kembalikan isi array & keluar dari function
-        return penumpang;
-      }
-    }
-  }
-}
+var angkot1 = new Angkot('Fahri Maulana', ['Bluru', 'Yosudarso', 'Pasar'],[],0);
+var angkot2 = new Angkot('Ari', ['Sidokare', 'Kidul', 'Gelam'],[],0);
+
+
