3fdf9fe568604a4754f46241c001c910bfad5163
FahViel <fahrimaulana007@gmail.com>
2023-01-01 21:21:03 +0700

    menambahkan code hasil belajar javascript execution context, hoisting & scope

---
+++ Javascript/latihan-18/script.js
@@ -0,0 +1,62 @@
+// 2.1 Execution Context, Hoisting & Scope
+
+// Creation Phase
+// console.log(nama);
+// var nama = 'fahri'
+// Execution Phase
+
+// Memakai Function
+// var nama = 'fahri'
+// var umur = '25'
+
+// function sayHello() {
+//   return `Halo, nama saya ${nama}, saya ${umur} tahun.`
+// }
+
+// console.log(sayHello())
+
+// var nama = 'Fahri'
+// var username = '@fah_viel'
+
+// function cetakURL() {
+//   console.log(arguments[0]);
+//   var instagramURL = 'http://instagram.com/'
+//   return instagramURL + username
+// }
+
+// console.log(cetakURL('@fahrimaulana'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function a() {
+//   console.log('ini a')
+
+//   function b() {
+//     console.log('ini b')
+
+//     function c() {
+//       console.log('ini c')
+//     }
+
+//     c()
+//   }
+
+//   b()
+// }
+
+// a()
