b36424b25193f468108baba58fff16b96d7a088d
FahViel <fahrimaulana007@gmail.com>
2023-01-01 21:48:41 +0700

    menambahkan kode hasil belajar javascript closures

--- Javascript/latihan-18/script.js
+++ Javascript/latihan-18/script.js
@@ -26,23 +26,6 @@
 
 // console.log(cetakURL('@fahrimaulana'));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // function a() {
 //   console.log('ini a')
 
@@ -60,3 +43,47 @@
 // }
 
 // a()
+
+// latihan Closures
+
+// function init() {
+//   // let nama = 'fahri'
+//   return function tampilNama(nama) {
+//     console.log(nama)
+//   }
+// }
+// let panggilNama = init()
+// panggilNama('fahri')
+
+
+// Function factories
+// function ucapkanSalam(waktu) {
+//   return function (nama) {
+//     console.log(`Halo ${nama}, Selamat ${waktu}, Semoga harimu menyenangkan`);
+//   }
+// }
+
+// let selamatPagi = ucapkanSalam('pagi')
+// let selamatSiang = ucapkanSalam('siang')
+// let selamatMalam = ucapkanSalam('malam')
+
+// selamatPagi('fahri')
+// selamatMalam('Maulana')
+
+// console.dir(selamatMalam)
+
+
+// Function Private
+
+let add = (function () {
+  let counter = 0
+
+  return function () {
+    return ++counter
+  }
+})()
+
+
+console.log(add());
+console.log(add());
+console.log(add());
