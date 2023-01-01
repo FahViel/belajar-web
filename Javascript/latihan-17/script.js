7f3fac5895d045b132330bfa154992216d040189
FahViel <fahrimaulana007@gmail.com>
2023-01-01 18:42:20 +0700

    Menambahkan code hasil belajar javascript prototype

--- Javascript/latihan-17/script.js
+++ Javascript/latihan-17/script.js
@@ -48,30 +48,81 @@
 // let fahri = new mahasiswa('Fahri', 10)
 
 // 4. Object.create()
-const methodMahasiswa = {
-  makan: function (porsi) {
-    this.energi += porsi
-    console.log(`Halo ${this.nama}, Selamat Makan`);
-  },
+// const methodMahasiswa = {
+//   makan: function (porsi) {
+//     this.energi += porsi
+//     console.log(`Halo ${this.nama}, Selamat Makan`);
+//   },
 
 
-  main: function (jam) {
-    this.energi -= jam
-    console.log(`Halo ${this.nama}, Selamat Bermain`);
-  },
+//   main: function (jam) {
+//     this.energi -= jam
+//     console.log(`Halo ${this.nama}, Selamat Bermain`);
+//   },
   
-  tidur: function (jam) {
-    this.energi += jam * 2
-    console.log(`Halo ${this.nama}, Selamat Bermain`);
-  }
-}
+//   tidur: function (jam) {
+//     this.energi += jam * 2
+//     console.log(`Halo ${this.nama}, Selamat Bermain`);
+//   }
+// }
 
-function mahasiswa(nama, energi) {
-    let mahasiswa = Object.create(methodMahasiswa)
-    mahasiswa.nama = nama
-    mahasiswa.energi = energi
+// function mahasiswa(nama, energi) {
+//     let mahasiswa = Object.create(methodMahasiswa)
+//     mahasiswa.nama = nama
+//     mahasiswa.energi = energi
 
-    return mahasiswa
-}
+//     return mahasiswa
+// }
 
-let fahri = mahasiswa('fahri Maulana', 10)
+// let fahri = mahasiswa('fahri Maulana', 10)
+
+
+
+// Versi Prototype Inheritance 
+
+// function mahasiswa(nama, energi) {
+//   this.nama = nama
+//   this.energi = energi
+// }
+// mahasiswa.prototype.makan = function (porsi) {
+//   this.energi += porsi
+//   return `Halo ${this.nama}, Selamat Makan`
+// }
+
+// mahasiswa.prototype.main = function (jam) {
+//   this.energi -= jam
+//   return `Halo ${this.nama}, Selamat Bermain`
+// }
+
+// mahasiswa.prototype.tidur = function (jam) {
+//   this.energi += jam * 2
+//   return `Halo ${this.nama}, Selamat Tidur`
+// }
+
+// let fahri = new mahasiswa('Fahri', 10)
+
+
+// Versi Class
+// class mahasiswa {
+//   constructor(nama, energi){
+//     this.nama = nama
+//     this.energi = energi
+//   }
+//   makan(porsi) {
+//     this.energi += porsi
+//     return `Halo ${this.nama}, Selamat Makan`
+//   }
+
+//   main(jam) {
+//     this.energi -= jam
+//     return `Halo ${this.nama}, Selamat Bermain`
+//   }
+
+//   tidur(jam) {
+//     this.energi += jam * 2
+//     return `Halo ${this.nama}, Selamat Tidur`
+//   }
+// }
+
+// let fahri = new mahasiswa('Fahri', 10)
+// let zidan = new mahasiswa('Zidan', 10)
