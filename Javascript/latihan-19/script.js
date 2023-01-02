a085de98caeb5a6d7545a06e5be4ac4dd4adfeed
FahViel <fahrimaulana007@gmail.com>
2023-01-02 18:17:21 +0700

    Menambahkan kode hasil belajar javascript : this pada arrow function

--- Javascript/latihan-19/script.js
+++ Javascript/latihan-19/script.js
@@ -83,6 +83,21 @@
 // }
 // const fahri = new Mahasiswa()
 
+// animation box
+const box = document.querySelector('.box')
+box.addEventListener('click', function () {
+    let satu = 'size'
+    let dua = 'caption'
+
+    if ( this.classList.contains(satu)) {
+        [satu, dua] = [dua, satu]   
+    }
+
+    this.classList.toggle(satu)
+    setTimeout(() => {
+        this.classList.toggle(dua)
+    }, 600)
+})
 
 
 
