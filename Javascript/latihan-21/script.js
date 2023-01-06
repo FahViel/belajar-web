20460c9a2001b14d7095e38caf872a63ebb003e6
FahViel <fahrimaulana007@gmail.com>
2023-01-06 22:07:50 +0700

    Menambahkan kode hasil latihan javascript : template literals (latihan)

--- Javascript/latihan-21/script.js
+++ Javascript/latihan-21/script.js
@@ -10,16 +10,95 @@
 // console.log(`${(x % 2 == 0) ? 'genap' : 'ganjil'}`);
 
 // HTML Fragment
+// const mhs = {
+//     nama : `Fahri`,
+//     umur : 25,
+//     nim : `191080200179`,
+//     email : `fahrimaulana007@gmail.com`
+// }
+
+// const el = `<div class="mhs">
+//     <h2>${mhs.nama}</h2>
+//     <span class="nrp">${mhs.nrp}</span>
+// </div>`
+
+// console.log(el);
+
+// Study Case HTML Fragment
+// 1. HTML Fragment
+// const mhs = {
+//     nama : `Fahri`,
+//     umur : 25,
+//     nim : `191080200179`,
+//     email : `fahrimaulana007@gmail.com`
+// }
+
+// const el = `<div class="mhs">
+//     <h2>${mhs.nama}</h2>
+//     <span class="nrp">${mhs.nim}</span>
+// </div>`
+
+// 2. Looping
+// const mhs = [
+//     {
+//     nama : `Fahri`,
+//     email : `fahrimaulana007@gmail.com`
+//     },
+
+//     {
+//     nama : `Zidan`,
+//     email : `zidan@gmail.com`
+//     }
+// ]
+
+// const el = `<div class="mhs">
+//     ${mhs.map( m => `<ul>
+//         <li>${m.nama}</li>
+//         <li>${m.email}</li>
+//     </ul>`).join('')}
+// </div>`
+
+// 3. Conditionals
+// Ternary
+// const lagu = {
+//     judul : 'Tetap dalam jiwa',
+//     penyanyi : 'Isyana',
+//     feat : `Rayi Putra`
+// }
+
+// const el = `<div class="lagu">
+//     <ul>
+//         <li>${lagu.penyanyi}</li>
+//         <li>${lagu.judul} ${lagu.feat ? `(feat. ${lagu.feat})` : ''}</li>
+//     </ul>
+// </div>`
+
+// 4. Nested
+// HTML Fragment Bersarang
 const mhs = {
-    nama : `Fahri`,
-    umur : 25,
-    nim : `191080200179`,
-    email : `fahrimaulana007@gmail.com`
+  nama: `Fahri`,
+  semester: 5,
+  mataKuliah : [
+    'Rekayasa Web',
+    `RPL`, 
+    `Pemrogaman sistem Interaktif`, 
+    `PBO`
+  ]
+}
+
+function cetakMataKuliah(mataKuliah) {
+  return `
+    <ol>
+      ${mataKuliah.map( mk => `<li>${mk}</li>`).join('')}
+    </ol>
+  `
 }
 
 const el = `<div class="mhs">
-    <h2>${mhs.nama}</h2>
-    <span class="nrp">${mhs.nrp}</span>
+  <h2>${mhs.nama}</h2>
+  <span class="semester">Semester : ${mhs.semester}</span>
+  <h4>Mata Kuliah :</h4>
+  ${cetakMataKuliah(mhs.mataKuliah)}
 </div>`
 
-console.log(el);
+document.body.innerHTML = el
