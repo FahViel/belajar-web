a0727ba1ea55398b995cf0ea84d9c16e7a21e2e0
FahViel <fahrimaulana007@gmail.com>
2023-01-13 20:48:52 +0700

    Menambahkan code hasil belajar pengenalan javascript : callback

---
+++ Javascript/latihan-26/script.js
@@ -0,0 +1,14 @@
+// Callback
+// Synchronus Callback
+// function halo(nama) {
+//     alert(`Halo, ${nama}`);
+// }
+
+// function tampilkanPesan(callback) {
+//     const nama = prompt(`Masukkan Nama : `)
+//     callback(nama)
+// }
+
+// tampilkanPesan(nama =>{
+//     alert(`Halo, ${nama}`)
+// } );
