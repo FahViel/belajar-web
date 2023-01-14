af6ac01d6d6795acaf6d594ba362b5b223f541fc
FahViel <fahrimaulana007@gmail.com>
2023-01-14 15:41:06 +0700

    Menambahkan code hasil latian javascript : callback (latihan) membuat web pencarian film

--- Javascript/latihan-26/script.js
+++ Javascript/latihan-26/script.js
@@ -12,3 +12,73 @@
 // tampilkanPesan(nama =>{
 //     alert(`Halo, ${nama}`)
 // } );
+$(`.search-button`).on(`click`, function () {
+
+  $.ajax({
+    url: `http://www.omdbapi.com/?apikey=420c5c51&s=` + $(`.input-keyword`).val(),
+    success: (result) => {
+      const movies = result.Search
+      let cards = ``
+  
+      movies.forEach((m) => {
+        cards += showCards(m)
+      })
+  
+      $(`.movie-container`).html(cards)
+  
+      // ketika tombol detail di klik
+      $(`.modal-detail-button`).on(`click`, function () {
+        $.ajax({
+          url: `http://www.omdbapi.com/?apikey=420c5c51&i=` + $(this).data('imdb-id'),
+          success: (m) => {
+            const movieDetail = showMovieDetail(m)
+            $('.modal-body').html(movieDetail)
+          },
+          error: (e) => {
+            console.log(e.responseText)
+          },
+        })
+      })
+    },
+    error: (e) => {
+      console.log(e.responseText)
+    },
+  })
+})
+
+function showCards(m) {
+  return `
+          <div class="col-md-4 my-3">
+              <div class="card">
+                <img src="${m.Poster}" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">${m.Title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                  <a href="#" class="btn btn-primary modal-detail-button"
+                  data-bs-toggle="modal" data-bs-target="#movieDetail" data-imdb-id="${m.imdbID}">Show Details</a>
+                </div>
+              </div>
+            </div>
+  `
+}
+
+function showMovieDetail(m) {
+  return `
+          <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-3">
+                  <img src="${m.Poster}" class="img-fluid">
+                </div>
+                <div class="col-md">
+                  <ul class="list-group">
+                    <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                    <li class="list-group-item"><strong>Director : ${m.Director}</strong></li>
+                    <li class="list-group-item"><strong>Actors : ${m.Actors}</strong></li>
+                    <li class="list-group-item"><strong>Writer : ${m.Writer}</strong></li>
+                    <li class="list-group-item"><Strong>Plot : </Strong><br>${m.Plot}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+`
+}
